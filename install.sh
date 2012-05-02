9b58ed7096923fb3f5907641836748f84f8f9e4c
Jonathan Camile <jonathan.camile+projects@gmail.com>
2012-05-02 14:32:38 +0200

    Added install file

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/bin ~/bin
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/dotfiles/pythonrc.py ~/.pythonrc.py
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
