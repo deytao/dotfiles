18e294327216f262fdadb47aa3c7a43eb5833a1d
Jonathan Camile <jonathan.camile+github@gmail.com>
2014-09-01 11:13:48 +0200

    Install: Add tmux folder

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/bin ~/bin
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/pythonrc.py ~/.pythonrc.py
+ln -s ~/dotfiles/tmux ~/.tmux
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
