bba6a5e0da8a574bab337b015d7ca9f87f855ae2
Jonathan Camile <jonathan.camile+github@gmail.com>
2018-10-17 16:15:38 +0000

    TMUX: Remove folder link

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/omz_custom ~/.omz_custom
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/pythonrc.py ~/.pythonrc.py
-ln -s ~/dotfiles/tmux ~/.tmux
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
