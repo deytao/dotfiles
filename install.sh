a8b04f65eca9c6f6f1f7404f6b31ce1280c2d22b
Jonathan Camile <jonathan.camile@arcanite.ch>
2024-10-09 14:53:56 +0200

    chore: clean install

--- install.sh
+++ install.sh
@@ -25,7 +25,6 @@ curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv
 
 curl -L git.io/antigen > ~/dotfiles/antigen.zsh
 [[ ! -L "${HOME}/.antigenrc" ]] && ln -s ~/dotfiles/antigenrc ~/.antigenrc
-[[ ! -L "${HOME}/bin" ]] && ln -s ~/dotfiles/bin ~/bin
 [[ ! -L "${HOME}/.config/alacritty" ]] && ln -s ~/dotfiles/alacritty ~/.config/alacritty
 [[ ! -L "${HOME}/.config/nvim" ]] && ln -s ~/dotfiles/nvim ~/.config/nvim
 [[ ! -L "${HOME}/.config/spaceship.zsh" ]] && ln -s ~/dotfiles/spaceship.zsh ~/.config/spaceship.zsh
@@ -72,6 +71,7 @@ my_install $1 \
 yay -S --noconfirm \
     balena-etcher \
     fd \
+    git-absorb \
     git-delta \
     glab \
     mattermost \
